6b108edaa4698a0076b4ad1ce6ece87dba7de2b2
Matt McCormick (thewtex) <matt@mmmccormick.com>
2010-10-31 15:35:08 -0500

    Strip trailing whitespace on histogram.cxx.

--- Code/histogram.cxx
+++ Code/histogram.cxx
@@ -14,7 +14,7 @@
 #include <vtkRenderWindowInteractor.h>
 #include <vtkSmartPointer.h>
 #include <vtkImageMagnitude.h>
- 
+
 int main( int argc, char *argv[] )
 {
   // Handle the arguments
@@ -23,25 +23,25 @@ int main( int argc, char *argv[] )
     std::cout << "Required arguments: filename.jpg" << std::endl;
     return EXIT_FAILURE;
     }
- 
+
   // Read a jpeg image
-  vtkSmartPointer<vtkImageReader2Factory> readerFactory = 
+  vtkSmartPointer<vtkImageReader2Factory> readerFactory =
     vtkSmartPointer<vtkImageReader2Factory>::New();
   vtkImageReader2 * reader = readerFactory->CreateImageReader2( argv[1] );
   reader->SetFileName( argv[1] );
   reader->Update();
- 
+
   vtkSmartPointer<vtkImageMagnitude> magnitude =
     vtkSmartPointer<vtkImageMagnitude>::New();
   magnitude->SetInputConnection(reader->GetOutputPort());
   magnitude->Update();
-  
+
   double red[3] = {1, 0, 0 };
- 
-  vtkSmartPointer<vtkIntArray> frequencies = 
+
+  vtkSmartPointer<vtkIntArray> frequencies =
     vtkSmartPointer<vtkIntArray>::New();
- 
-  vtkSmartPointer<vtkImageAccumulate> histogram = 
+
+  vtkSmartPointer<vtkImageAccumulate> histogram =
     vtkSmartPointer<vtkImageAccumulate>::New();
   histogram->SetInputConnection(magnitude->GetOutputPort());
   histogram->SetComponentExtent(0,255,0,0,0,0);
@@ -58,49 +58,49 @@ int main( int argc, char *argv[] )
     {
     frequencies->SetTuple1(j, *output++);
     }
- 
-  vtkSmartPointer<vtkDataObject> dataObject = 
+
+  vtkSmartPointer<vtkDataObject> dataObject =
     vtkSmartPointer<vtkDataObject>::New();
- 
+
   dataObject->GetFieldData()->AddArray( frequencies );
- 
+
   // Create a vtkBarChartActor
-  vtkSmartPointer<vtkBarChartActor> barChart = 
+  vtkSmartPointer<vtkBarChartActor> barChart =
     vtkSmartPointer<vtkBarChartActor>::New();
- 
+
   barChart->SetInput(dataObject);
   barChart->SetTitle("Histogram");
   barChart->GetPositionCoordinate()->SetValue(0.05,0.05,0.0);
   barChart->GetPosition2Coordinate()->SetValue(0.95,0.85,0.0);
   barChart->GetProperty()->SetColor(1,1,1);
- 
+
   barChart->GetLegendActor()->SetNumberOfEntries(dataObject->GetFieldData()->GetArray(0)->GetNumberOfTuples());
   barChart->LegendVisibilityOff();
   barChart->LabelVisibilityOff();
- 
+
   int count = 0;
   for(int i = 0; i < 256; ++i)
     {
     barChart->SetBarColor( count++, red );
     }
- 
+
   // Visualize the histogram
-  vtkSmartPointer<vtkRenderer> renderer = 
+  vtkSmartPointer<vtkRenderer> renderer =
     vtkSmartPointer<vtkRenderer>::New();
   renderer->AddActor(barChart);
- 
-  vtkSmartPointer<vtkRenderWindow> renderWindow = 
+
+  vtkSmartPointer<vtkRenderWindow> renderWindow =
     vtkSmartPointer<vtkRenderWindow>::New();
   renderWindow->AddRenderer(renderer);
   renderWindow->SetSize(640, 480);
- 
+
   vtkSmartPointer<vtkRenderWindowInteractor> interactor =
     vtkSmartPointer<vtkRenderWindowInteractor>::New();
   interactor->SetRenderWindow(renderWindow);
- 
+
   // Initialize the event loop and then start it
   interactor->Initialize();
   interactor->Start();
- 
+
   return  EXIT_SUCCESS;
 }
